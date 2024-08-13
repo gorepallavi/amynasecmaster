bd0d088e46ee99cc934b8f1bc4b66f021617ccd0
gorepallavi <gorepallavi464@gmail.com>
2024-08-13 17:05:38 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import Service from './Service';
 import Blogs from './Blogs';
 import Event from './Event';
 import Conferancetalks from './Conferancetalks';
+<<<<<<< HEAD
 import Training from './Training';
+=======
+import Training from'./Training';
+>>>>>>> 5740ed7 (Initial commit)
 import Community from './Community';
 import About from './About';
 import Singleservicepage from './Single-servicepage';
@@ -16,6 +20,7 @@ import Wast from './Wast';
 import Mast from './Mast';
 import Redteaming from './Redteaming';
 import Api from './Api';
+<<<<<<< HEAD
 import Contact from './Contact';
 
 ReactDOM.render(
@@ -37,6 +42,34 @@ ReactDOM.render(
       <Route path="/redteaming" element={<Redteaming />} />
       <Route path="/api" element={<Api />} />
       <Route path="/contact" element={<Contact />} />
+=======
+import Contact from'./Contact';
+
+
+
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/services" element={<Service />} />
+      <Route path="/Blogs" element={<Blogs />} />
+      <Route path="/events" element={<Event />} />
+      <Route path="/conference-Talks" element={<Conferancetalks />} />
+      <Route path="/Training" element={<Training />} />
+      <Route path="/Community" element={<Community />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Single-servicepage" element={<Singleservicepage />} />
+      <Route path="/IOT Security Assesment" element={<IOTsecurity />} />
+      <Route path="/Information Security Assesment" element={<InformationSecurity />} />
+      <Route path="/Wast" element={<Wast />} />
+      <Route path="/Mast" element={<Mast />} />
+      <Route path="/Redteaming" element={<Redteaming />} />
+      <Route path="/Api" element={<Api />} />
+      <Route path="/Contact" element={<Contact />} />
+      
+
+
+>>>>>>> 5740ed7 (Initial commit)
     </Routes>
   </Router>,
   document.getElementById('root')
